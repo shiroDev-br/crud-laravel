42b6e212afc2dbf9c71febf37bb2008b0c5860de
shiroDev-br <arthurdevcomercial@gmail.com>
2025-07-30 20:27:08 -0300

    feat: register posts route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
@@ -20,5 +21,9 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
 });
+
+Route::resource('posts', PostController::class)
+    ->only(['index', 'create', 'store', 'edit', 'show', 'update', 'destroy'])
+    ->names('posts');
 
 require __DIR__.'/auth.php';
